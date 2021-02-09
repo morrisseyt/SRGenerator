462499e941d46e2146ac4e30417944f377cb4b74
J <jcraczkiewicz@icloud.com>
2021-02-09 11:46:54 -0500

    testing scapy on captured pcaps

--- myfirstscapy.py
+++ myfirstscapy.py
@@ -1,30 +1,76 @@
-#!/usr/bin/env python3
+! /usr/bin/env python3
 
 import sys
 from scapy.all import *
 
-# Read pcap from stdin
+### below packages used for steps 2 - 3
+#from prettytable import PrettyTable
+#from collections import Counter
+
+### Read pcap from stdin
 
 data = sys.argv[1]
 packets = rdpcap(data)
 
-# Count and print the number of packets in the pcap
-# count = 0 
+### Check for TCP/IP layer
+
 # for packet in packets:
-#       count +=1
+#	if (packet.haslayer(ICMP)):
+#         print(f"ICMP code: {packet.getlayer(ICMP).code}")
+
+### Count packets in pcap
+
+# count = 0
+# for packet in packets:
+#	count +=1
 # print (count)
 
-# print session streams 
+### Print session streams 
+
 # sessions = packets.sessions()
 # for session in sessions:
-#        print (session)
+#	 print (session)
 
-# returns command that wld generate the packet 
+### Returns command that wld generate the packet in question
+
 #  for packet in packets:
 #        print (packet.command())
 
-# fills a format string with fields values of the packet
-# for packet in packets:
-#       print (packet.sprintf)
+### Fills a format string with fields values of the packet
 
+# for packet in packets:
+#	print (packet.sprintf)
 # print (packets.summary)
+
+
+### Step 1: Loop to print IPs in packet
+
+#for packet in packets:
+#	if IP in packet:
+#		try:
+#			print(packet[IP].src)
+#		except:
+#			pass
+
+### Step 2: Read and append
+
+#srcIP = []
+#for packet in packets:
+#	if IP in packet:
+#		try:
+#			srcIP.append(packet[IP].src)
+#		except:
+#			pass
+
+### Step 3: Count
+
+#count=Counter()
+#for ip in srcIP:
+#	count[ip] += 1
+
+### Step 4: Table and Print 
+
+#table = PrettyTable(["IP", "Count"])
+#for ip, count in count.most_common():
+#	table.add_row([ip, count])
+#print(table)
