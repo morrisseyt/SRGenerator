5df1934c5571503361a256b37bc3008a2b0c6b5c
J <jcraczkiewicz@icloud.com>
2021-02-10 16:13:47 -0500

    Modified to include five ANSI color code options

--- pktCount_colorTester.py
+++ pktCount_colorTester.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
-import sys
-from scapy.all import *
+# import sys
+# from scapy.all import *
 
 ### below packages used for steps
 # from prettytable import PrettyTable
@@ -9,28 +9,28 @@ from scapy.all import *
 
 ### Read pcap from stdin
 
-data = sys.argv[1]
-packets = rdpcap(data)
+# data = sys.argv[1]
+# packets = rdpcap(data)
 
 ### Packet counter
-count = 0
-for packet in packets:
-        count +=1
-
-# Printing colored text by creating function using Python's format() method  
-
-def prRed(txt): print("\033[1;31m {}\033[00m".format(txt))  # bold red
-
-prRed(f'This pcap has {count} packets')
+# count = 0
+# for packet in packets:
+#        count +=1
 
 # Printing colored text by setting variable to desired ANSI code(s)
 
-YH = '\033[30;43m' # black text, yellow background
-R = '\033[1;31m' # bold red text
-WB = '\033[1;37;46m' # bold white text on cyan backgroun
+BR = '\033[1;31m' # bold red text
+BW = '\033[1;37m' # bold white text
+BB = '\033[1;34m' # bold blue text
+BG = '\033[1;32m' # bold green
+BY = '\033[1;33m' # bold yellow
 
-print(YH + f'This pcap has {count} packets')
+# YH = '\033[30;43m' # black text, yellow background
+# WB = '\033[1;37;46m' # bold white text on cyan backgroun
 
-print(R + 'Same count, different color')
+print(BR + 'Sample bold red text')
+print(BW + 'Same bold white text')
+print(BB + 'Sample bold blue')
+print(BG + 'Sample bold green')
+print(BY + 'Sample bold yellow')
 
-print(WB + 'This time in blue')
